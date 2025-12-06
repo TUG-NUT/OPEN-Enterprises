6f2654934f995ec81703d96e996a7608b3f54480
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2025-12-05 22:19:53 -0700

    new store

---
+++ data/destination_paradise.js
@@ -0,0 +1,324 @@
+data = [
+    {
+        "title": "Eldritch Spinner",
+        "cost": 200,
+        "quantity": 1,
+        "overview": "An arcane artifact to keep you focused.",
+        "description": "When you spin the Eldritch Spinner as a bonus action, you can continue to concentrate on a spell for an additional turn when your concentration would have otherwise ended. It can be used a number of times equal to your spellcasting ability modifier, with its uses restoring during a long rest.</p><p>The Eldritch Spinner also serves as an arcane focus.<br>",
+        "img": "./images/spinner.jpg",
+        "visible": false
+    },
+    {
+        "title": "Gray Duck Vodka",
+        "cost": 450,
+        "quantity": 1,
+        "overview": "100 Ducks, That's Our Promise",
+        "description": "Each handcrafted, ornate, glass bottle of Gray Duck Vodka contains 100 ducks, that's our promise. <br> <p style='text-align: center;'>Do Not Ingest.</p>",
+        "img": "./images/Gray Duck.jpg",
+        "visible": true
+    },
+    {
+        "title": "Parental Lock",
+        "cost": 100,
+        "quantity": 1,
+        "overview": "An arcane padlock.",
+        "description": "A large padlock that, once secured, can only be unlocked by one of the parents of the person who locked it.<br>",
+        "img": "./images/padlock.jpg",
+        "visible": false
+    },
+    {
+        "title": "Horse Hat",
+        "cost": 500,
+        "quantity": 1,
+        "overview": "The easiest way to transport your horse.",
+        "description": "This item allows you to store your horse on top of your head without having to make a strength roll. It doesn't conceal the horse; you can still see it if you squint.",
+        "img": "./images/horsehat.png",
+        "visible": true
+    },
+    {
+        "title": "Horn of Resounding Triumph",
+        "cost": 200,
+        "quantity": 1,
+        "overview": "A battle horn of victory.",
+        "description": "When you sound the Horn of Resounding Triumph as an action in battle, you fill your companions with inspiration. Each of your allies within 100 ft. of you gain a bardic inspiration die (<b>1d6</b>) for the duration of combat.</p><p>If you sound the horn in a confined space, every creature in a 100 ft. radius must make a <b>DC 14 </b>Constitution saving throw, taking <b>2d6</b>&nbsp;thunder damage and being deafened on a failed save, and half as much damage (and not deafened) on a successful save. This item can be used once per long rest.",
+        "img": "./images/horn.jpeg",
+        "visible": true
+    },
+    {
+        "title": "Ring of Sobriety",
+        "cost": 50,
+        "quantity": 1,
+        "overview": "A ring that keeps you clear-headed.",
+        "description": "When wearing this ring, you are immune to the hazy, clumsy, and sluggish side effects from partaking in alcohol or drugs. However, the moment the ring is removed from your finger, all of the cumulative effects that were held off by the ring come crashing down on you at once.",
+        "img": "./images/ring1.webp",
+        "visible": true
+    },
+    {
+        "title": "Spectacles of Truthsight",
+        "cost": 1500,
+        "quantity": 1,
+        "overview": "The easiest way to see through the bullshit.",
+        "description": "When wearing these dichromatic spectacles, you can visually separate the truth or falsehood of anyone's speech by color. Truth appears to you as green; lies appear to you as red, and partial truths appear somewhere in between. They can be used for up to 10 minutes; afterwards, roll 1d6. On a 1, the glasses fracture beyond repair.",
+        "img": "./images/spectaclesrg.gif",
+        "visible": false
+    },
+    {
+        "title": "Hammer of Surmise Sturdiness",
+        "cost": 150,
+        "quantity": 1,
+        "overview": "A hammer to help you find what's hidden.",
+        "description": "This magic hammer, when lightly struck against a surface, will produce a soft&nbsp;<i>tap</i>&nbsp;sound that rings differently depending on the material it's being struck against and the thickness of the material. For the first 3 feet of the material, the user can infer the material type, including open air if the surface being struck is less than 3 feet. For each additional foot after that, the bearer of the hammer must make an Intelligence (Arcana) check of DC 10 + the number of feet greater than 3 to determine what material is there and whether there is an opening. For example, 4 feet would have a DC of 11, 5 feet a DC of 12, and so on. Until they fail this check, they can continue trying to discern the material farther and farther, one foot at a time.</p><p>This hammer has a limited number of uses equal to 1d4 per day.",
+        "img": "./images/hammer.jpg",
+        "visible": false
+    },
+    {
+        "title": "Shield of Faith",
+        "cost": 100,
+        "quantity": 1,
+        "overview": "A shield that only works if you believe",
+        "description": "Whenever you take the <b>Dodge</b>&nbsp;action, you can choose to make a <b>DC 15 Religion (WIS)</b>&nbsp;check in order to believe this shield will protect you. On a success, your <b>AC</b>&nbsp;is increased by 5 until the start of your next turn.",
+        "img": "./images/shieldoffaith.jpeg",
+        "visible": false
+    },
+    {
+        "title": "Book of Forgetfulness",
+        "cost": 300,
+        "quantity": 1,
+        "overview": "What?",
+        "description": "Whenenever you open this book, make a DC 15 Intelligence save. On a failure, you forget everything you've done for the last 1d8 minutes (including opening the book).",
+        "img": "./images/bookblank.jpg",
+        "visible": true
+    },
+    {
+        "title": "Bocce Ball Set",
+        "cost": 125,
+        "quantity": 1,
+        "overview": "For the Italian in all of us",
+        "description": "A bag containing 4 sets of 2 heavy balls of varying colors. To activate, throw the first ball of a set to a desired location. From there, two effects will be available to the thrower.</p><p><br></p><p>1. The exact distance between the 2 balls is known to the thrower.</p><p>2. The holder of the second ball has the option to teleport to the thrown ball. However, teleporting from one ball to the other expends that set of bocce balls.",
+        "img": "./images/bocceballs.jpg",
+        "visible": true
+    },
+    {
+        "title": "Mirror of Scrying",
+        "cost": 500,
+        "quantity": 1,
+        "overview": "Get a sneak peak",
+        "description": "This handheld mirror lets you see things that would otherwise not be possible to see. Once per day, you may attempt to use this mirror by rolling 1d6. The outcome is based on the result of the roll:<ol><li>The mirror cannot show you what you seek because the knowledge is forbidden. It shatters and cannot be repaired.</li><li>The mirror cannot find what you seek. You may try again tomorrow.</li><li>The mirror shows you something from the past.</li><li>The mirror shows you something from the present.</li><li>The mirror shows you something from the future.</li><li>You choose to see something from the past, present, or future.</li></ol>Glimpses of the future may not be set in stone.",
+        "img": "./images/mirrorscrying.jpg",
+        "visible": true
+    },
+    {
+        "title": "Silly String",
+        "cost": 150,
+        "quantity": 1,
+        "overview": "A nice little halloween item",
+        "description": "Once per day, you can use this can to make a ranged attack against a creature (if you're proficient with any crossbow, you're proficient with this can). On a hit, the target is blinded. The range for this weapon is 15/60.<br>Alternatively, you can spend one minute spraying a 15 ft. radius circle using this can. This area becomes difficult terrain for up to 8 hours.",
+        "img": "./images/silly-string.jpg",
+        "visible": true
+    },
+    {
+        "title": "Tarantula Bracelet",
+        "cost": 500,
+        "quantity": 1,
+        "overview": "Become a spider-man",
+        "description": "<i>Requires Attunement</i><br>Once per day, the bracelet bestows <i><a href='https://dnd5e.wikidot.com/spell:spider-climb'>Spider Climb</a></i> on the wearer for ten minutes. Additionally, the wearer's movement while climbing is completely silent and gives them advantage on stealth checks while the effect is active.",
+        "img": "./images/tarantulabracelet.jpg",
+        "visible": true
+    },
+    {
+        "title": "Grammarian's Ring",
+        "cost": 200,
+        "quantity": 1,
+        "overview": "A ring to correct your spellings",
+        "description": "Once per adventure, you can alter one letter in a spell title and cast it, altering the effect to match its new title. For example, <i>Cause Fear</i> could become <i>Cause Bear</i>, summoning a bear to the battlefield. However, the ring can never be used to alter the same spell in the same way twice.<br>After using this ring, roll a d20. On a 1, the ring loses its charges, but can be recharged by a skilled mage for half of the ring's cost in gold.",
+        "img": "./images/rubyring.jpg",
+        "visible": true
+    },
+    {
+        "title": "Ring of Misty Step",
+        "cost": 425,
+        "quantity": 1,
+        "overview": "",
+        "description": "<i>Requires Attunement</i><br>Once per day, the wearer of this ring can cast <i><a href='https://dnd5e.wikidot.com/spell:misty-step'>Misty Step</a></i>.",
+        "img": "./images/silver-ring.avif",
+        "visible": false
+    },
+    {
+        "title": "Ring of Movement",
+        "cost": 450,
+        "quantity": 1,
+        "overview": "",
+        "description": "<i>Requires Attunement</i><br>The wearer's movement is increased by 10 ft.",
+        "img": "./images/ring-green.jpg",
+        "visible": true
+    },
+    {
+        "title": "Haunted Doll",
+        "cost": 100,
+        "quantity": 1,
+        "overview": "A very creepy doll",
+        "description": "If the owner of this doll ever fails a third death save, the doll will take the hit, and will die in place of its owner.",
+        "img": "https://images.squarespace-cdn.com/content/v1/59b4328f49fc2b50d08701fb/1542239970729-KL7M3DYIICCCYSM0UCCU/creepy_baby_dolls",
+        "visible": true
+    },
+    {
+        "title": "Fists of Fury",
+        "cost": 500,
+        "quantity": 0,
+        "overview": "Sick as hell knuckle gloves",
+        "description": "<i>Requires Attunement</i><br>These fingerless gloves have \"G4M3 0V3R\" written across the knuckles. When you make an unarmed strike, the damage is increased by 1d4. Additionally, the target must make a Strength saving throw or be pushed back 10 ft. The DC for this save is your passive Athletics score.",
+        "img": "https://www.meme-arsenal.com/memes/6131a796f6d21694af29ec89b1306fcb.jpg",
+        "visible": true
+    },
+    {
+          "title": "Bear Trap",
+        "cost": 600,
+        "quantity": 1,
+        "overview": "Hey, Bear!",
+        "description": "A tripwire trap that, when activated, spawns a bear",
+        "img": "./images/bear oven.jpg",
+        "visible": true
+    },    
+    {
+        "title": "Wand of Switcheroo",
+        "cost": 625,
+        "quantity": 1,
+        "overview": "",
+        "description": "When pointed at a creature of similar size (no more than one size category smaller or larger than you) within 120 ft., the holder will switch places with that target, if the target is willing. If the target is unwilling, it must succeed a DC 13 Constitution save to resist. One charge per long rest.",
+        "img": "https://5e.tools/img/items/DMG/Wand%20of%20Polymorph.webp",
+        "visible": true
+    },
+    {
+        "title": "Gauntlets of Healing",
+        "cost": 150,
+        "quantity": 1,
+        "overview": "Helps to give you a better rest",
+        "description": "<i>Requires Attunement</i><br>Enhances your healing factor. Whenever taking a short rest, you're able to heal an additional 1d6 health. Additionally, you can choose a nearby ally to heal an additional 1d4 health during the same rest.",
+        "img": "https://www.bandagesplus.com/pub/media/catalog/product/cache/2f5f8cbdb2dbaf6da38028b1b2cb1c0a/e/x/exostrong_gauntlet_black.jpg",
+        "visible": true
+    },
+    {
+        "title": "Fizzy Lifting Drinks",
+        "cost": 250,
+        "quantity": 1,
+        "overview": "Do not steal.",
+        "description": "Drinking a bottle from this 4-pack gives you the ability to float around. For the next 1 minute, you have a flying speed of 10 ft.",
+        "img": "https://wicked-good.co/cdn/shop/products/FizzyLiftingDrink-WillyWonkaScent.png?v=1759356407&width=1080",
+        "visible": true
+    },
+    {
+        "title": "Anti-Gravity Sphere",
+        "cost": 500,
+        "quantity": 1,
+        "overview": "A fist-sized ball of glass.",
+        "description": "This small orb is filled with silvery smoke. When the orb is destroyed, it disables the effect of gravity for everything within a 30 ft. radius.",
+        "img": "https://www.shutterstock.com/shutterstock/videos/1070332816/thumb/1.jpg?ip=x480",
+        "visible": false
+    }, 
+    {
+        "title": "All-or-Nothing Coin",
+        "cost": 100,
+        "quantity": 1,
+        "overview": "Risk it for the biscuit",
+        "description": "Once per long rest, instead of rolling a d20 on an attack roll or skill check, you can flip a coin. A heads is a critical hit; a tails is a critical fail.",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/ATTICA%2C_Athens._Circa_545-525-15_BC.jpg/250px-ATTICA%2C_Athens._Circa_545-525-15_BC.jpg",
+        "visible": true
+    },
+    {
+        "title": "Gray Duck Vodka",
+        "cost": 100,
+        "quantity": 1,
+        "overview": "100 Ducks, That's Our Promise",
+        "description": "Each handcrafted, ornate, glass bottle of Gray Duck Vodka contains 100 ducks, that's our promise. <br> <p style='text-align: center;'>Do Not Ingest.</p>",
+        "img": "https://tug-nut.github.io/OPEN-Enterprises/images/Gray%20Duck.jpg",
+        "visible": true
+    },
+    {
+        "title": "Jester's Hat",
+        "cost": 475,
+        "quantity": 1,
+        "overview": "A silly cap",
+        "description": "<i>Requires Attunement</i><br>A bard wearing this hat has one additional use of bardic inspiration. Additionally, when giving bardic inspiration to an ally, as soon as that ally expends that inspiration, both the ally and the wearer are healed for 1d4 health.",
+        "img": "https://images.halloweencostumes.com/products/69125/2-1-161454/court-jester-multicolor-soft-hat-alt-1-update.jpg",
+        "visible": true
+    },
+    {
+        "title": "Mockingbird Gum",
+        "cost": 300,
+        "quantity": 2,
+        "overview": "Green-and-blue speckled minty sticks",
+        "description": "A five-pack of chewing gum. Upon chewing one of these sticks, you're able to perfectly emulate another person's voice for up to an hour.",
+        "img": "https://www.longmontcompletedentistry.com/blog/wp-content/uploads/2023/04/AdobeStock_43878704__1680914959_50471.jpg",
+        "visible": true
+    },
+    {
+        "title": "Spellkeeper Cards",
+        "cost": 275,
+        "quantity": 1,
+        "overview": "A strange deck with no face cards",
+        "description": "This deck of cards has a magical enchantment that permits you to sometimes restore a spell slot. Once per day, as an action you may draw a card from this deck. Choose the level of spell slot you'd like to restore, then roll a d10 to determine the value of the card. If the spell slot's level is lower than the card value, the slot is restored.",
+        "img": "https://rareplayingcards.com/cdn/shop/products/ultimatedeck_a2071195-b8d5-4cf0-b11c-21643d0ae368.jpg?v=1675864853",
+        "visible": true
+    },
+    {
+        "title": "Vibe Check Glasses",
+        "cost": 250,
+        "quantity": 1,
+        "overview": "Lets you read the room",
+        "description": `<i>Requires attunement</i><br>These tinted glasses allow you insight into the current vibes. As an action, you can use them to ask the DM one of the following, to which they will respond with a one-word answer (good, bad, strange, etc.): 
+        <ul>
+        <li>What the current vibe in the room is</li>
+        <li>What the vibe is from a particular person present</li>
+        <li>What the vibe is from a particular direction (such as northwest, uptairs, down a corridor, etc.)</li>
+        </ul>
+        Altnernatively, you can ask the DM what the nearest source of good/bad vibes is.
+        <br><br>The glasses have up to four charges, and each question asked uses a charge. On a long rest, the glassess have a 50% chance of regaining a charge. If unused for four days, they regain all charges automatically.`,
+        "img": "https://store.lagunitas.com/cdn/shop/products/IPASunglasses_1200x600_crop_center.png?v=1668725556",
+        "visible": true
+    },
+    {
+        "title": "Plot Armor",
+        "cost": 150,
+        "quantity": 3,
+        "overview": `You should always wear protection`,
+        "description": `This thin, paper-light armor can be worn underneath any other standard set of armor. When you are hit for damage that would drop you to zero health, the armor absorbs the hit instead, nullifying the damage dealt. However, this cracks the armor, making it ineffective and unrepairable.`,
+        "img": "https://dl2.myminifactory.com/object-assets/644ff9f46b1e86.87395864/images/720X720-screenshot-1421.jpg",
+        "visible": true
+    },
+    {
+        "title": "Sport Mode Crocs",
+        "cost": 325,
+        "quantity": 3,
+        "overview": `Strap in`,
+        "description": `Once per long rest, you can move the straps on these versatile footwear to behind your heels, setting them to sport mode. For the next 10 minutes, you can move across water surfaces without sinking. However, you must be running to gain this benefit. If you stop or slow to a walking pace, you sink into the water as normal.`,
+        "img": "https://m.media-amazon.com/images/I/715O-+0xHcL._AC_UY900_.jpg",
+        "visible": true
+    },
+    {
+        "title": "Assassin's Cloak",
+        "cost": 500,
+        "quantity": 1,
+        "overview": ``,
+        "description": `<i>Requires attunement</i><br>Once per short rest, when you kill a creature, you can choose to turn invisible. The invisibility has the same effect as the <a href='https://roll20.net/compendium/dnd5e/Invisibility#content'>Invisibility</a> spell.`,
+        "img": "https://m.media-amazon.com/images/I/31ytxnhsZeL._AC_UY1000_.jpg",
+        "visible": false
+    },
+    {
+        "title": "The Dark Web",
+        "cost": 750,
+        "quantity": 1,
+        "overview": `You dont want to know what's there anyways...`,
+        "description": `A large webbing that can cover a passage equal or smaller than 12' x 12'. The webbing is an unnaturally dark shadow, and when a creature tries to walk through it, they come out the other side on the same side they entered. `,
+        "img": "./images/matrix text.webp",
+        "visible": true
+    },
+    {
+        "title": "Sample",
+        "cost": 0,
+        "quantity": 0,
+        "overview": ``,
+        "description": ``,
+        "img": "./images/",
+        "visible": false
+    }            
+]
